dee7a6686c26f01f94c6476856aa089ef170b903
Peter <peter.vicman@gmail.com>
2025-01-23 20:08:25 +0100

    libnfs: update to 7afd17d

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnfs"
-PKG_VERSION="6.0.2"
-PKG_SHA256="4e5459cc3e0242447879004e9ad28286d4d27daa42cbdcde423248fad911e747"
+PKG_VERSION="7afd17d094c8e564bf13e2dfe7bc60dc2e4fada8"
+PKG_SHA256="4a375b1bd3cc6b4ff2b5527044fe35acde260e43927087ee4efdcee95082ee0f"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="https://github.com/sahlberg/libnfs"
-PKG_URL="https://github.com/sahlberg/libnfs/archive/libnfs-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/sahlberg/libnfs/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A client library for accessing NFS shares over a network."
 PKG_TOOLCHAIN="autotools"
