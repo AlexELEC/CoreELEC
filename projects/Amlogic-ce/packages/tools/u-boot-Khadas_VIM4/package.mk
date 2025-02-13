e11cea4bac1a236a8cbd65bac6eb8765dd829428
Portisch <hugo.portisch@yahoo.de>
2025-02-13 16:48:29 +0100

    u-boot-Khadas_VIM4: bump package to 4a160151

--- projects/Amlogic-ce/packages/tools/u-boot-Khadas_VIM4/package.mk
+++ projects/Amlogic-ce/packages/tools/u-boot-Khadas_VIM4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="u-boot-Khadas_VIM4"
-PKG_VERSION="e481a1d405dfae0c0562df8720ac131f9731c75e"
-PKG_SHA256="0c6edb8e8153ffd20bb9e4749907164aefdf582320936bf6e5185e9f891ae97b"
+PKG_VERSION="4a16015168d00d08487781fc914e048944554c6e"
+PKG_SHA256="433e4a01f2aa3c409714eb46191aca0b56a821f6d04220f8dbf2f84d389c803c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="https://github.com/CoreELEC/u-boot/archive/${PKG_VERSION}.tar.gz"
