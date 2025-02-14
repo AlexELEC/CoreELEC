ab30b3d79126d583a5dccce6c3393bdaa403df2d
Portisch <hugo.portisch@yahoo.de>
2025-02-14 11:45:39 +0100

    Revert "common_drivers: bump package to f65a08e0"
    This reverts commit a8a45d4e31a1145485e6d571864e1b76ac3ec7b5.

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="f65a08e06a6eabe90a12261d14c38e0ddfb4abc1"
-PKG_SHA256="0d5d2a12aac42f7712a8f0e253303a58c0333dbce539b0ee2173d555a3d0a1cb"
+PKG_VERSION="8a3cad01ac63e38b807f2ffeaf2666b08923e928"
+PKG_SHA256="f05ddf3736e73ae1fb8be548d07f79431c1f2273488b6575cbc24732086e571a"
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/common_drivers/archive/${PKG_VERSION}.tar.gz"
