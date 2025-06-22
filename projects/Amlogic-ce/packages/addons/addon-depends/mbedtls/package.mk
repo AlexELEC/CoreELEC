e431923a0f5ff08b6f1e18d05d9c9a8a3ec7278d
Peter <peter.vicman@gmail.com>
2025-06-22 09:08:37 +0200

    mbedtls: bump package to 3.6.3.1

--- projects/Amlogic-ce/packages/addons/addon-depends/mbedtls/package.mk
+++ projects/Amlogic-ce/packages/addons/addon-depends/mbedtls/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="mbedtls"
-PKG_VERSION="3.6.2"
-PKG_SHA256="a3c959773bc5d5b22353bc605e96d92fae2eac486dcaf46990412b84a1a0fb5f"
+PKG_VERSION="3.6.3.1"
+PKG_SHA256="243ed496d5f88a5b3791021be2800aac821b9a4cc16e7134aa413c58b4c20e0c"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/Mbed-TLS/mbedtls"
-PKG_URL="https://github.com/Mbed-TLS/mbedtls/archive/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/Mbed-TLS/mbedtls/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain jsonschema:host Jinja2:host"
 PKG_DEPENDS_UNPACK="mbedtls-framework"
 PKG_LONGDESC="Mbed TLS is a C library that implements cryptographic primitives, X.509 certificate manipulation and the SSL/TLS and DTLS protocols."
