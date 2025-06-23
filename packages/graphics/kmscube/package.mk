891742ef8c76edbddd2258b4bb8b40d77986169b
Portisch <hugo.portisch@yahoo.de>
2025-06-23 12:46:34 +0200

    kmscube: bump package to 311eaaaa

--- packages/graphics/kmscube/package.mk
+++ packages/graphics/kmscube/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kmscube"
-PKG_VERSION="9f63f359fab1b5d8e862508e4e51c9dfe339ccb0"
-PKG_SHA256="896ac76671a9b89836a7014b16cc85b45b041e03fe34a8f529f4718aa2b15cef"
+PKG_VERSION="311eaaaa473d593c30d118799aa19ac4ad53cd65"
+PKG_SHA256="ae07899cd9cfbdff37f28a49de3d1f2e463534073004c4915ce404923eb2d0c1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gitlab.freedesktop.org/mesa/kmscube"
 PKG_URL="https://gitlab.freedesktop.org/mesa/kmscube/-/archive/master/kmscube-${PKG_VERSION}.tar.gz"
